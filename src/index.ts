560016cc93caff1eaf6cccb5448661cfbeed2292
Magical_ball <1355061380@qq.com>
2026-01-06 11:43:38 +0800

    [fix]修复跨域问题

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import router from './routes'
 
 const app = new Hono()
 
+app.use(
+  '/api/*',
+  cors({
+    origin: 'http://localhost:3000/',
+    credentials: true,
+  })
+)
 app.route('/api', router)
 
 export default {
